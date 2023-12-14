402985a324c007ad84e15504cdf9b93fc5cb7021
Fadl Lul Hakim Ihsan <48132237+HakimIhsan@users.noreply.github.com>
2023-12-14 16:55:29 +0000

    pull login changed

---
+++ src/pages/formbank/Bank.jsx
@@ -0,0 +1,25 @@
+function Bank() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="Octocat.png" className="App-logo" alt="logo" />
+          <p>
+            Ini Halaman Bank <span className="heart">♥️</span> React
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Bank;
+  
